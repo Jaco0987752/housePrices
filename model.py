d52ab2bd08b3161f9d0c97c74f530e69cdd12cc9
Jaco0987752 <0987752@hr.nl>
2022-12-07 13:12:31 +0100

    Reading database

---
+++ model.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  7 12:57:44 2022
+
+@author: Jaco
+"""
+
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+data = pd.read_csv("Complete_dataset.csv", sep=';')

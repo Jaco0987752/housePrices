cdaed4dbe6517fb604df45a5b2aa22a3b66400ea
Jaco0987752 <0987752@hr.nl>
2023-01-12 18:15:48 +0100

    Converting elastic ned to lasso

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ Created on Wed Dec  7 12:57:44 2022
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.linear_model import ElasticNet
+from sklearn.linear_model import Ridge
 from sklearn.model_selection import train_test_split
 
 data = pd.read_csv("data.csv", sep=';')
@@ -24,7 +24,7 @@ data["huishoudengrote"] = pd.to_numeric(data["GemiddeldeHuishoudensgrootte_89"])
 
 
 data["inkomen"] = pd.to_numeric(data["inkomen"])
-print(data[["inkomen", "Jaar", "Hypotheekrente"]])
+#print(data[["inkomen", "Jaar", "Hypotheekrente"]])
 
 data["borrowcapacity"] = data["inkomen"] / data["Hypotheekrente"]
 data["housepressure"] = data["Bevolkingsdichtheid_57"] / data["Woningdichtheid_93"]  
@@ -32,7 +32,7 @@ data["housepressure"] = data["Bevolkingsdichtheid_57"] / data["Woningdichtheid_9
 #data["borrowcapacity"] = data["loon"] / np.sqrt(data["Hypotheekrente"])
 
 
-city = 'Rotterdam'
+city = 'Rotterdam' #'Rotterdam'#'Amsterdam'#
 
 # Get the values of gemeente and sort them on year.
 gemeente = data[data["RegioS_Title"] == city]
@@ -53,7 +53,7 @@ x_test, x_train, y_test, y_train, = train_test_split(
 
 
 # Do linear regression.
-reg = ElasticNet(alpha=1.0,  l1_ratio=0.5)
+reg = Ridge(alpha=15.0)
 reg.fit(x_train, y_train)
 y_pred = reg.predict(x_values)
 #y_pred2 = reg.predict(X_train)
@@ -61,16 +61,16 @@ y_pred = reg.predict(x_values)
 x_train = np.array(x_train)
 y_train = np.array(y_train)
 
-print(y_train)
+#print(y_train)
 
 x_test_ = np.array(x_test)
 y_test_ = np.array(y_test)
 
-print(x_test)
+#print(x_test)
 
 plt.title("housing prices " + city)
 plt.ylabel("Average sales prices")
-plt.xlabel("mortgage rate")
+plt.xlabel("borrow capacity")
 plt.plot(np.array(x_values), y_pred, color="blue", label="regressionLine")
 plt.scatter(x_train, y_train, color="red", label="training_data")
 plt.scatter(x_test_, y_test_, color="yellow", label="test_data")
